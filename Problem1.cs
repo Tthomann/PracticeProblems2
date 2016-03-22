ba55135905e93117c855c0d019712a97831d6cb7
Tyler Thomann <Tyler Thomann>
2016-03-22 15:44:41 -0500

    first problem complete

---
+++ Problem1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems2
+{
+    public class InputCheck
+    {
+        List<string> CollectiveInput = new List<string>();
+        string userInput;
+        public InputCheck()
+        {
+
+        }
+        public string UserInput()
+        {
+            
+            return userInput;
+        }
+        public void CheckList()
+        {
+            
+            CollectiveInput.Add(" ");
+
+            for (int checker = 0; checker < CollectiveInput.Count; checker++)
+            {
+                Console.WriteLine("enter a word");
+                userInput = Console.ReadLine();
+
+                if(CollectiveInput[checker] == userInput)
+                {
+                    Console.WriteLine("BARK, THATS ALREADY BEEN ENTERED BARK");
+                    Console.ReadKey();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("youuuuu did it!");
+                    CollectiveInput.Add(userInput);
+                    
+                    
+                }
+            }
+        
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems2
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            bool TylerisCool = true;
+            InputCheck inputCheck = new InputCheck();
+            
+            while(TylerisCool)
+            {
+                //inputCheck.UserInput();
+                inputCheck.CheckList();
+            }
+        }
+    }
+}
